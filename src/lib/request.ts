1d099fddcad43fda6b4731226a5bc4363131c354
Konstantint Pukki <KPukki@alfabank.ru>
2024-04-12 16:50:49 +0300

    feat: add parseRules in request

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -11,9 +11,13 @@ const API_HOST = process.env.USE_MOCKS
   ? process.env.SERVER_MOCKS_HOST
   : process.env.API_HOST;
 
+export type RequestInitWithParseRule = RequestInit & {
+  parseRule?: "json" | "text";
+};
+
 export async function request<T extends Object>(
   url: string,
-  init?: RequestInit
+  { parseRule = "json", ...init }: RequestInitWithParseRule = {}
 ): Promise<T | RequestError> {
   try {
     const session = getSession();
@@ -27,7 +31,7 @@ export async function request<T extends Object>(
       },
     });
 
-    const data = (await res.json()) as T;
+    const data = (await (parseRule === "json" ? res.json() : res.text())) as T;
 
     if (!res.ok) {
       return {
@@ -50,18 +54,40 @@ export async function request<T extends Object>(
   }
 }
 
-export async function get<T extends Object>(url: string) {
-  return request<T>(url);
+export async function get<T extends Object>(
+  url: string,
+  init?: RequestInitWithParseRule
+) {
+  return request<T>(url, init);
 }
 
-export async function post<T extends Object>(url: string, body: Object) {
-  return request<T>(url, { method: "POST", body: JSON.stringify(body) });
+export async function post<T extends Object>(
+  url: string,
+  body: Object,
+  init?: RequestInitWithParseRule
+) {
+  return request<T>(url, {
+    method: "POST",
+    body: JSON.stringify(body),
+    ...init,
+  });
 }
 
-export async function patch<T extends Object>(url: string, body: Object) {
-  return request<T>(url, { method: "PATCH", body: JSON.stringify(body) });
+export async function patch<T extends Object>(
+  url: string,
+  body: Object,
+  init?: RequestInitWithParseRule
+) {
+  return request<T>(url, {
+    method: "PATCH",
+    body: JSON.stringify(body),
+    ...init,
+  });
 }
 
-export async function remove<T extends Object>(url: string) {
-  return request<T>(url, { method: "DELETE" });
+export async function remove<T extends Object>(
+  url: string,
+  init?: RequestInitWithParseRule
+) {
+  return request<T>(url, { method: "DELETE", ...init });
 }
